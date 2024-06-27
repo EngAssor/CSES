0c0b0ced17c36c1cbaaf358ac5ec7fab2ffa074e
Asser Tantawy <Aserfs2000@gmail.com>
2024-06-27 05:34:21 +0300

    solve trailing zeros

--- Trailing Zeros.cpp
+++ Trailing Zeros.cpp
@@ -1,33 +1,41 @@
+/*
+Your task is to calculate the number of trailing zeros in the factorial n!.
+For example, 20!=2432902008176640000 and it has 4 trailing zeros.
+Input
+The only input line has an integer n.
+Output
+Print the number of trailing zeros in n!.
+Constraints
+
+1 \le n \le 10^9
+
+Example
+Input:
+20
+
+Output:
+4
+
+*/
 #include<iostream>
 using namespace std;
 typedef unsigned long long ll;
 
- ll fact(ll n)
-{
-    ll c=0;
-    ll i=n;
-    ll FACT=1;
-    for(;i>1;i--)
-    {
-        FACT*=i;
-
-     //   cout<<"i IS = "<<i<<endl;
-        if(FACT%10 == 0)
-          {
-              c++;
-       // cout<<"C is ="<<c<<endl;
-
-        FACT/=10;
-          }
-          FACT
-     //   cout<<"Fact is  ="<<FACT<<endl;
-    }
-return c;
-}
-
 int main(){
-ll n=0;
+
+ll n=0  ;
 cin>>n;
-cout<<fact(n);
-return 0;
+
+ll count=0; 
+//i want to clacute the trailing zeros
+
+for(ll i=5;i<=n;i*=5){
+  count+=n/i; 
+
+}   
+
+cout<<count;
+
+  
+
 }
